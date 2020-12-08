c5f79045ed4771d815da050e813c1d84f4936e0e
rahul <rahulpattanshetty@gmail.com>
2020-12-09 00:27:06 +0530

    assignment 1 does basic math

---
+++ spec/assignments/1_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe "Math Calculations" do
+  
+  it "does addition operation" do
+    expect(4+4).to eq(8)
+  end
+
+  it "does minus operation" do
+    expect(4-4).to eq(0)
+  end
+  
+  it "does multiplication operation" do
+    expect(4*4).to eq(16)
+  end
+
+  it "does division operation" do
+    expect(4/4).to eq(1)
+  end
+
+end
