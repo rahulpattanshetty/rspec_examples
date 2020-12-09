d14869d909615cf572ed6194898115b930236692
rahul <rahulpattanshetty@gmail.com>
2020-12-09 12:42:28 +0530

    Helper method - Hooks, custom methods

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -10,13 +10,22 @@ end
 
 RSpec.describe "Card" do
 
+  # Hooks are a piece of code that automatically executes at a specific time during the test case excution.
+  # before hook is used run at the start and after hook is used to run at the end of the example execution.
+  # before do
+  #   @card = Card.new("Ace","Shades")
+  # end
+
+  # Custom method which can be called inside the "it" block
+  def card
+    Card.new("Ace","Shades")
+  end
+
   it "has a rank" do
-    card = Card.new("Ace", "Shades")
     expect(card.rank).to eq("Ace")
   end
 
   it "has a suit" do
-    card = Card.new("Ace", "Shades")
     expect(card.suit).to eq("Shades")
   end
 
