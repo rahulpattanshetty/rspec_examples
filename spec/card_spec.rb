2778c1c45c8c5eaaa73260db4e8ea06fddfa9d78
rahul <rahulpattanshetty@gmail.com>
2020-12-09 13:19:06 +0530

    let method and memoryization

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -17,9 +17,17 @@ RSpec.describe "Card" do
   # end
 
   # Custom method which can be called inside the "it" block
-  def card
-    Card.new("Ace","Shades")
-  end
+  # def card
+  #   Card.new("Ace","Shades")
+  # end
+
+  # Memoryization:- End of the day it works like a caching. Computer will figure it once and refer you back to the first computational result whenever called. (So it basically refere to the let method)
+  # Let method will act as new method for every examples but if it is called more than one time in an example then it only refers to computation that is made for the first time and return you the result by referring it.
+  # let method will act as "Lazy loading" because it is initialized when it is called.
+  let(:card) { Card.new("Ace","Shades") }
+  # let(:x) { 1 + 1 }
+  # let(:y) { x + 1 }
+  # let!(:card) { Card.new("Ace","Shades") } # let with bang will act before hook
 
   it "has a rank" do
     expect(card.rank).to eq("Ace")
@@ -29,4 +37,9 @@ RSpec.describe "Card" do
     expect(card.suit).to eq("Shades")
   end
 
+  it "has a custom error message" do
+    comparsion = "Shade"
+    # expect method accept two parameter, one is matcher and other one is error message
+    expect(card.suit).to eq(comparsion), "Hey, expected #{comparsion} but got #{card.suit}"
+  end
 end
