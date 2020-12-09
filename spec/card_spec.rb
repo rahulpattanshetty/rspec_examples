22b318a681d6babdf05f692951f46d4fad613d95
rahul <rahulpattanshetty@gmail.com>
2020-12-09 12:21:25 +0530

    assignment 2 school

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,8 +1,23 @@
+class Card
+  attr_reader :rank, :suit
+
+  def initialize(rank,suit)
+    @rank = rank
+    @suit = suit
+  end
+
+end
+
 RSpec.describe "Card" do
-  
-  it "has a type" do
-    card = Card.new("Ace of shades")
-    expect(card.type).to eq("Ace of shades")
+
+  it "has a rank" do
+    card = Card.new("Ace", "Shades")
+    expect(card.rank).to eq("Ace")
+  end
+
+  it "has a suit" do
+    card = Card.new("Ace", "Shades")
+    expect(card.suit).to eq("Shades")
   end
 
 end
