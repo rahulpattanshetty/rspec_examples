c2f955855bd12d9d071becc84bfb5c11b45c168e
rahul <rahulpattanshetty@gmail.com>
2020-12-11 00:34:28 +0530

    Shared examples with include_examples

---
+++ spec/shared_examples_spec.rb
@@ -0,0 +1,32 @@
+# shared examples can be used across multiple example groups
+# subject inside shared example will be lazy loaded it refer to a called example described class
+RSpec.shared_examples "Ruby object which returns length" do
+  it 'should return the length of an object' do
+    expect(subject.length).to eq(3)
+  end
+end
+
+RSpec.describe Array do
+  subject { [1,2,3] }
+  include_examples "Ruby object which returns length"
+end
+
+RSpec.describe Hash do
+  subject { {a:1, b:2, c:3} }
+  include_examples "Ruby object which returns length"
+end
+
+RSpec.describe String do
+  subject { "abc"}
+  include_examples "Ruby object which returns length"
+end
+
+class SausageLink
+  def length
+    3
+  end
+end
+
+RSpec.describe SausageLink do
+  include_examples "Ruby object which returns length"
+end
